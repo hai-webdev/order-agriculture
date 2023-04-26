34ff418d9110a3819834ec7ec6bfa69e290f885d
zb_hai <hai_web@163.com>
2023-04-26 13:36:28 +0800

    初次提交

---
+++ src/js/styles~about~advantage~contact~index~news~news_view~product~product_view.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{6:function(n,o,w){},7:function(n,o,w){}}]);
--- src/pages/common/footer/index.js
+++
@@ -1,5 +0,0 @@
-// 页尾的js代码
-import "./index.less";
-$(".to-top").on("click", function(){
-    $("html,body").animate({scrollTop:0},1000)
-})
